3823279c3fb3397ce6aa713ee25dfa15c282ca43
Tobias <eydam-prototyping@outlook.com>
2024-12-26 11:48:54 +0100

    Stub added

---
+++ src/epCC1101/stubs/spidev.py
@@ -0,0 +1,13 @@
+class SpiDev:
+    max_speed_hz = 0
+    def __init__(self):
+        pass
+
+    def open(self, bus, cs):
+        pass
+
+    def xfer2(self, data):
+        return [0, 0]
+    
+    def close(self):
+        pass
